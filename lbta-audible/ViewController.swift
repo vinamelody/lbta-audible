1d4fdc5fedbcebf770eccecd40b09cbf2c97f3a4
Vina Melody <vina_melody@yahoo.com>
2017-01-07 19:42:14 +0800

    Initial Commit

---
+++ lbta-audible/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lbta-audible
+//
+//  Created by Vina Melody on 7/1/17.
+//  Copyright © 2017 Vina Rianti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
