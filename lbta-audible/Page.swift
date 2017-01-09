ece592d8200e96b4fa31e6808257286d7b9aa07f
Vina Melody <vina_melody@yahoo.com>
2017-01-09 22:21:11 +0800

    Initialize page struct and data

---
+++ lbta-audible/Page.swift
@@ -0,0 +1,15 @@
+//
+//  Page.swift
+//  lbta-audible
+//
+//  Created by Vina Melody on 9/1/17.
+//  Copyright © 2017 Vina Rianti. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    let title: String
+    let message: String
+    let imageName: String
+}
--- lbta-audible/ViewController.swift
+++ lbta-audible/ViewController.swift
@@ -23,6 +23,15 @@ class ViewController: UIViewController {
     }()
     
     let cellId = "cellId"
+    
+    let pages: [Page] = {
+        let firstPage = Page(title: "Share a great listen", message: "It's free to send your books to the people in your life. Every recipient's first book is on us.", imageName: "page1")
+        let secondPage = Page(title: "Send from your library", message: "Tap the More menu next to any book. Choose \"Send this Book\"", imageName: "page2")
+        let thirdPage = Page(title: "Send from the player", message: "Tap the More menu in the upper corner. Choose \"Send this Book.\"", imageName: "page3")
+        
+        return [firstPage, secondPage, thirdPage]
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,7 +51,7 @@ class ViewController: UIViewController {
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return pages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
