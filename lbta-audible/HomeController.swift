8f4273ac674d2b4994de42b7e4d385fa2ce83b11
Vina Melody <vina_melody@yahoo.com>
2017-01-21 23:07:16 +0800

    Add handleSignOut

---
+++ lbta-audible/HomeController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeController.swift
+//  lbta-audible
+//
+//  Created by Vina Melody on 21/1/17.
+//  Copyright © 2017 Vina Rianti. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "We're logged in"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Sign Out", style: UIBarButtonItemStyle.plain, target: self, action: #selector(handleSignOut))
+        
+        let imageView = UIImageView(image: UIImage(named: "home"))
+        view.addSubview(imageView)
+        _ = imageView.anchor(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 64, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+    }
+    
+    func handleSignOut() {
+        UserDefaults.standard.set(false, forKey: "isLoggedIn")
+        UserDefaults.standard.synchronize()
+        
+        // Note: sometimes, the simulator doesn't exactly save the value properly. Becareful with that. In the actual device though it's working
+        
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+}
